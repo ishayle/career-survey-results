f94fbcbd2e664e9f04a0ada996d43bdb178845fd
ishayle <ishayle@gmail.com>
2022-05-08 09:25:47 +0300

    1 file modified, 1 added, 1 deleted

--- src/app/survey-data.service.ts
+++ src/app/survey-data.service.ts
@@ -1,32 +1,37 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs';
 
 @Injectable()
 export class SurveyDataService {
   public carrerResultArray: CarrerResult[] = [];
   constructor(private http: HttpClient) {
-    this.http.get('/survey_data.csv', { responseType: 'text' }).subscribe(
-      (data) => {
-        let csvToRowArray = data.split('\n');
-        for (let index = 1; index < csvToRowArray.length; index++) {
-          let row = csvToRowArray[index].split(',');
-          this.carrerResultArray.push(new CarrerResult(row[0], row[1], row[2]));
+    this.http
+      .get('/assets/survey_data.csv', { responseType: 'text' })
+      .subscribe(
+        (data) => {
+          let csvToRowArray = data.split('\n');
+          for (let index = 1; index < csvToRowArray.length; index++) {
+            let row = csvToRowArray[index].split(',');
+            this.carrerResultArray.push(
+              new CarrerResult(index, row[1], row[2])
+            );
+          }
+          console.log(this.carrerResultArray);
+        },
+        (error) => {
+          console.log(error);
         }
-        console.log(this.carrerResultArray);
-      },
-      (error) => {
-        console.log(error);
-      }
-    );
+      );
   }
 }
 
 export class CarrerResult {
-  id: String;
+  id: number;
   name: String;
   lastName: String;
 
-  constructor(id: String, name: String, lastName: String) {
+  constructor(id: number, name: String, lastName: String) {
     this.id = id;
     this.name = name;
     this.lastName = lastName;
