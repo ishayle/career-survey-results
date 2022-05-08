1521a984d76d2795d41bdcdd6d9157031b52cb53
ishayle <ishayle@gmail.com>
2022-05-08 11:20:12 +0300

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,12 +9,56 @@ import { CarrerResult, SurveyDataService } from './survey-data.service';
 export class AppComponent {
   name = 'Angular ' + VERSION.major;
   public carrerResultArray: CarrerResult[] = [];
-
-  constructor(private surveyDataService: SurveyDataService) {
-    
-  }
+  public dynamicResults = [];
+  width: number = 700;
+  height: number = 300;
+  fitContainer: boolean = false;
+  view: [700, 300];
+  showXAxis = true;
+  showYAxis = true;
+  gradient = true;
+  showLegend = true;
+  showXAxisLabel = true;
+  xAxisLabel = 'Country';
+  showYAxisLabel = true;
+  yAxisLabel = 'Sales';
+  timeline = true;
+  doughnut = true;
+  colorScheme = {
+    domain: ['#9370DB', '#87CEFA', '#FA8072', '#FF7F50', '#90EE90', '#9370DB'],
+  };
+  //pie
+  showLabels = true;
+  constructor(private surveyDataService: SurveyDataService) {}
   public ngOnInit(): void {
     this.carrerResultArray = this.surveyDataService.carrerResultArray;
+    this.dynamicResults = [
+      {
+        name: 'reason1',
+        series: [
+          {
+            name: 'male',
+            value: 2243772,
+          },
+          {
+            name: 'female',
+            value: 1227770,
+          },
+        ],
+      },
+      {
+        name: 'reason2',
+        series: [
+          {
+            name: 'male',
+            value: 23772,
+          },
+          {
+            name: 'female',
+            value: 1770,
+          },
+        ],
+      },
+    ];
   }
-
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,17 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { SurveyDataService } from './survey-data.service';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    NgxChartsModule,
+    BrowserAnimationsModule,
+  ],
   declarations: [AppComponent],
   providers: [SurveyDataService],
   bootstrap: [AppComponent],
